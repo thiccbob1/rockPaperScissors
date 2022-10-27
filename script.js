39826819e1e124ffa3f48dc94c8a99ecd4f0a005
thiccbob1 <samplejesse1@gmail.com>
2022-10-27 17:54:05 -0500

    added switch for computer choice and user input function and button

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 let computerChoice;
+let computerChoiceString;
+let userChoice;
+let button = document.getElementById('submit');
 
 function computerChoiceGen(min, max){
     computerChoice = Math.floor(Math.random() * (max - min) + min);
-    console.log(computerChoice)
-}
+    switch (computerChoice){
+        case 1: 
+        computerChoiceString = ("Rock");
+        break;
 
-computerChoiceGen(1, 4);
+        case 2: 
+        computerChoiceString = ("Paper");
+        break;
+
+        case 3: 
+        computerChoiceString = ("Scissors");
+        break;
+
+        default: 
+        console.log("Error");
+    }
+    console.log(computerChoiceString);
+};
+
+function choice() {
+    userChoice = document.getElementById('userChoice').value;
+    userChoice = userChoice.toUpperCase();
+    console.log(userChoice);
+};
+
+button.addEventListener('click', choice);
+button.addEventListener('click', computerChoiceGen(1, 4));
