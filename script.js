1735a95a1b20c74a8fa97e632cdc3f788ec6bde0
thiccbob1 <samplejesse1@gmail.com>
2022-10-27 18:05:18 -0500

    added numberi cvalue to user input and commented code

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let computerChoice;
 let computerChoiceString;
 let userChoice;
+let userChoiceString;
 let button = document.getElementById('submit');
 
+
+//Generates a number and string for the computer's choice,
+//then logs it to console
 function computerChoiceGen(min, max){
     computerChoice = Math.floor(Math.random() * (max - min) + min);
     switch (computerChoice){
@@ -24,10 +28,22 @@ function computerChoiceGen(min, max){
     console.log(computerChoiceString);
 };
 
+//Read user's choice, then converts it to a number and upper
+//case string, the logs to console
 function choice() {
-    userChoice = document.getElementById('userChoice').value;
-    userChoice = userChoice.toUpperCase();
+    userChoiceString = document.getElementById('userChoice').value;
+    userChoiceString = userChoiceString.toUpperCase();
+    if (userChoiceString == ("ROCK")){
+        userChoice = 1;
+    }else if (userChoiceString == ("PAPER")){
+        userChoice = 2;
+    } else if (userChoiceString == ("SCISSORS")){
+        userChoice = 3;
+    } else {
+        console.log("User typing error pls make code for this");
+    }
     console.log(userChoice);
+    console.log(userChoiceString);
 };
 
 button.addEventListener('click', () => {
